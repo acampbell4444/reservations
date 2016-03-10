a1e929161fd85d9b466542d5ffaaf7824158e813
Alan Campbell <alancampbell4444@gmail.com>
2016-03-10 09:21:24 -0800

    yay

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -55,7 +55,7 @@ class ReservationsController < ApplicationController
       formatted_plus_4 = (now_plus_4.strftime("%m-%d-%Y").to_s)
       formatted_plus_4
       if formatted_plus_4 >= @reservation.date && current_user.standard?
-        flash[:alert] = "Reservation was not Saved! Online reservations must be made by no later than 8pm on the day before the activity. No online reservation dates
+        flash[:alert] = "Reservation was not Saved! Online reservations must be made by no later than 8pm on the day before the activity. Also, no online reservation dates
         can be later than #{Playday.last.date}. To reserve by phone call 310-510-1777."
         return redirect_to :back
       end
